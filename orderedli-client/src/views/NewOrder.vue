66bf439bcd204faff8e4202110a1786be4125f1f
Cameron Briner <cambriner@gmail.com>
2020-12-22 17:00:03 -0600

    creating vue front end

---
+++ orderedli-client/src/views/NewOrder.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    New Order
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+@Options({
+  components: {
+    HelloWorld
+  }
+})
+export default class NewOrder extends Vue {}
+</script>
